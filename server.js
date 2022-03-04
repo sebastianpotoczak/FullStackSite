33f171d5653bb472ea20ab52320405f1bcf5f7f5
Sebastian Potoczak <sebastianpotoczak3@wp.pl>
2022-03-04 02:29:19 +0100

    Done

--- server.js
+++ server.js
@@ -14,15 +14,7 @@ require("dotenv").config({path: "./config.env"})
  
 const port = process.env.PORT || 1337;
 
-if(process.env.NODE_ENV === "production"){
-	app.use(express.static(path.join(__dirname, "build")));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, "build", "index.html"))
-	});
-}else{
-	app.get('/', (req, res) =>{
-		res.send("Api running")
-	})
+
 }
 
 
@@ -53,7 +45,7 @@ app.use(cors())
 app.use(express.json())
 
 
-mongoose.connect('mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(process.env.PORT || 'mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 }),
