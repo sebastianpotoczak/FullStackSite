e460c23636b8e9a71e04939b9a15654b2002092c
Sebastian Potoczak <sebastianpotoczak3@wp.pl>
2022-03-15 23:34:22 +0100

    done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ function App() {
 	async function loginUser(event) {
 		event.preventDefault()
 
-		const response = await fetch('https://beauty-app-pl.herokuapp.com/api/login', {
+		const response = await fetch('http://localhost:1337/api/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
--- server.js
+++ server.js
@@ -1,3 +1,125 @@
+// const express = require('express')
+// const path = require("path");
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+// const User = require('./models/user.model')
+// const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcryptjs')
+// const jsonServer = require('json-server')
+// const server =  jsonServer.create();
+// const router = jsonServer.router('db.json')
+// require("dotenv").config({path: "./config.env"})
+
+
+
+
+// app.use(express.static(path.join(__dirname, "/client/build")))
+
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
+
+
+
+// const middlewares = jsonServer.defaults({
+// 	static: '/build'
+// })
+
+// server.use(middlewares);
+// server.use(jsonServer.rewriter({
+// 	"/events/*": '/$1'
+// }))
+
+// server.use(router)
+
+
+// server.use((req, res, next) => {
+// 	if (req.method === 'POST') {
+// 	  req.body.createdAt = Date.now()
+// 	}
+// 	next()
+//   })
+
+//   server.listen(3005, () => {
+// 	console.log('JSON Server is running')
+//   })
+
+
+// app.use(cors())
+// app.use(express.json())
+
+// mongoose.connect("mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+// 	useUnifiedTopology: true,
+// 	useNewUrlParser: true
+// }),
+
+// app.post('/api/register', async (req, res) => {
+// 	console.log(req.body)
+// 	try {
+// 		const newPassword = await bcrypt.hash(req.body.password, 10)
+// 		await User.create({
+// 			name: req.body.name,
+// 			surname: req.body.surname,
+// 			phone: req.body.phone,
+// 			email: req.body.email,
+// 			password: newPassword,
+// 			admin: false
+// 		})
+// 		res.json({ status: 'ok' })
+// 	} catch (err) {
+// 		res.json({ status: 'error', error: 'Duplicate email' })
+// 	}
+// })
+
+// app.post('/api/login', async (req, res) => {
+// 	const user = await User.findOne({
+// 		email: req.body.email,
+// 	})
+
+
+// 	if (!user) {
+// 		return { status: 'error', error: 'Invalid login' }
+// 	}
+
+// 	const isPasswordValid = await bcrypt.compare(
+// 		req.body.password,
+// 		user.password
+// 	)
+
+// 	if (isPasswordValid) {
+// 		const token = jwt.sign(
+// 			{
+// 				name: user.name,
+// 				email: user.email,
+// 			},
+// 			'secret123'
+// 		)
+// 		const phone = jwt.sign({
+// 			phones: user.phone
+// 		}, 'secret123')
+
+// 		const surname = jwt.sign({
+// 			surname: user.surname
+// 		}, 'secret123')
+// 		const admin = jwt.sign({
+// 			admin: user.admin
+// 		}, 'secret123')
+
+// 		return res.json({ status: 'ok', user: token, phones: phone, surname: surname, admin: admin})
+// 	} else {
+// 		return res.json({ status: 'error', user: false })
+// 	}
+// })
+
+
+
+// app.listen(1337, () => console.log(`Server listening on port ${port}`));
+
+
+
+
 const express = require('express')
 const path = require("path");
 const app = express()
@@ -11,18 +133,10 @@ const server =  jsonServer.create();
 const router = jsonServer.router('db.json')
 require("dotenv").config({path: "./config.env"})
 
+ 
+const port = process.env.PORT || 1337;
 
 
-const port = process.env.PORT || 2337;
-
-
-
-app.use(express.static(path.join(__dirname, "/client/build")))
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
 
 
 
@@ -34,7 +148,6 @@ server.use(middlewares);
 server.use(jsonServer.rewriter({
 	"/events/*": '/$1'
 }))
-
 server.use(router)
 
 
@@ -53,7 +166,8 @@ server.use((req, res, next) => {
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+
+mongoose.connect(process.env.PORT || 'mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 }),
@@ -116,6 +230,38 @@ app.post('/api/login', async (req, res) => {
 	}
 })
 
+app.get('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		const user = await User.findOne({ email: email })
+
+		return res.json({ status: 'ok', quote: user.quote })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
+app.post('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
 
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
