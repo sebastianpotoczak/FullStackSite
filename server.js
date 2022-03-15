c4d6f44cd6bd10ff3314f67d95910b811018763c
Sebastian Potoczak <sebastianpotoczak3@wp.pl>
2022-03-15 13:38:37 +0100

    some

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcryptjs')
 const jsonServer = require('json-server')
 const server =  jsonServer.create();
 const router = jsonServer.router('db.json')
-require("dotenv").config({path: "./config.env"})
+require("dotenv").config({path: "./.env"})
 
 
 const port = process.env.PORT || 1337;
@@ -42,7 +42,7 @@ app.use(cors())
 app.use(express.json())
 
 
-mongoose.connect('mongodb+srv://sebastian:database123@cluster0.h7hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URL,{
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 }),
